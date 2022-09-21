b0dac592d1318ac32678cc8dce1c7c2f847113ae
TopVirusMinus <mwmma5000@gmail.com>
2022-09-21 16:59:17 +0200

    Added style to custom select

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Select from "./Select";
+
+const options = [
+  { label: "First Option", value: 1 },
+  { label: "Second Option", value: 2 },
+  { label: "Third Option", value: 3 },
+  { label: "Fourth Option", value: 4 },
+  { label: "Fifth Option", value: 5 },
+];
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Select options={options} />
     </div>
   );
 }
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,6 +1,8 @@
+import CSS from "./select.module.css";
+
 type SelectOption = {
   label: string;
-  value: string;
+  value: any;
 };
 
 type SelectProps = {
@@ -8,8 +10,25 @@ type SelectProps = {
   options: SelectOption[];
   onChange: (value: SelectOption | undefined) => void;
 };
+
 const Select = ({ value, onChange, options }: SelectProps) => {
-  return <input type="select" />;
+  return (
+    <div tabIndex={0} className={CSS.container}>
+      <div className={CSS.value}>Value</div>
+      <button className={CSS["clear-btn"]}>&times;</button>
+      <button className={CSS.divider}></button>
+      <button className={CSS.caret}></button>
+      <div className={CSS.options}>
+        {options.map((option) => {
+          return (
+            <li key={option.value} className={CSS.option}>
+              {option.label}
+            </li>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Select;
